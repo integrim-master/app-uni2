e3cbeea966e37c5301ae9c50a233f2076a9e6923
Andres <mr.coco1420@gmail.com>
2026-02-24 14:26:43 -0500

    Feature: agregar interfaz de usuario para el blog con detalles y lista de artículos

---
+++ app/blog/_layout.tsx
@@ -0,0 +1,41 @@
+import { useTheme } from "@/src/context/ThemeContext";
+import HeaderGradient from "@/src/ui/HeaderGradient";
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function BlogLayout() {
+  const { colors } = useTheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerTitleAlign: "center",
+        headerTintColor: "white",
+        headerShadowVisible: false,
+        headerTransparent: false,
+        header: ({ navigation, options, back }) => (
+          <HeaderGradient title={options.title as string} back={!!back} />
+        ),
+      }}
+    >
+      <Stack.Screen
+        name="[details]/index"
+        options={{
+          title: "Perfil",
+          headerShadowVisible: true,
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="index"
+        options={{
+          // title: "Tus solicitudes",
+          // headerShadowVisible: false,
+          headerShown: false,
+          animation: "ios_from_left",
+        }}
+      />
+    </Stack>
+  );
+}
